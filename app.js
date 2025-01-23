ddf8f57ea04e912245d4c7fdcff7114064d07b27
LeoPasq <leopasq10@yahoo.com.br>
2025-01-23 20:59:08 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// JOGO DO LEO DE XADREZ
+alert('Boas Vindas ao site do Leo');
+let nome = prompt('Qual o seu nome?');
+let somaPecas = 0;
+let valMax = 500;
+let numAleatorio =  parseInt(Math.random() * valMax +1);
+alert(`${nome}, voce ganhara pontos pelas peças de xadrez que inserir. A cada jogada lhe direi seus pontos`);
+while (somaPecas < numAleatorio) {
+let pecaXadrez = prompt('escolha uma peça de xadrez').toLowerCase();
+if (pecaXadrez == 'rei') {
+   somaPecas += 200;
+} else 
+   if (pecaXadrez == 'dama') {
+       somaPecas += 90;
+}  else 
+      if (pecaXadrez == 'torre') {
+         somaPecas += 50;
+}     else 
+         if (pecaXadrez == 'bispo') {
+            somaPecas += 40;
+}        else 
+               if (pecaXadrez == 'cavalo') {
+                  somaPecas += 30;
+}              else 
+                  if (pecaXadrez == 'peão') {
+                     somaPecas += 10;
+}                 else {
+                     somaPecas -= 50;
+}
+alert(`${nome}, a soma dos pontos é: ${somaPecas}`);
+}
+alert(`Parabéns ${nome}!!`);
+alert(`Voce somou mais que ${numAleatorio} e venceu o jogo!`);
